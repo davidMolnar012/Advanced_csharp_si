ea3f8ee3aea82dfe3552a9cb2ad86a4338d000e6
David <davmmm@gmail.com>
2020-03-26 15:22:04 +0100

    Finish 5th task

---
+++ 1_si/exercise_5/SortString/SortString/SortString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SortString
+{
+    class SortString
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            Console.WriteLine(str);
+            string[] words = str.Split(' ');
+            Console.WriteLine(string.Join(", ", words));
+            Array.Sort(words);
+            Console.WriteLine(string.Join(", ", words));
+            string s = string.Join(" ", words);
+            Console.WriteLine(s);
+        }
+    }
+}
